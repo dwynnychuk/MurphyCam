01b938d0284fa43805ce2d068fcd8c0665c3075e
dwynnychuk <92692830+dwynnychuk@users.noreply.github.com>
2022-04-24 17:48:34 -0600

    Added functionality to access webcam

---
+++ main.py
@@ -0,0 +1,20 @@
+# Initial script to run camera
+# April 2022
+
+# import
+from tabnanny import check
+import cv2
+
+# access camera and display image. ESC key to stop
+cam = cv2.VideoCapture(0)
+
+while True:
+    check, frame = cam.read()
+    cv2.imshow('Video', frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
